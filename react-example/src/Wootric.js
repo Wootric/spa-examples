328f4ce7b60b02aff22478a0cd5785457793bafb
diegoserranoa <mail@diegoserranoa.com>
2020-06-09 11:40:41 -0500

    Update React example

--- react-example/src/Wootric.js
+++ react-example/src/Wootric.js
@@ -5,6 +5,7 @@ class Wootric extends React.Component {
   componentDidMount() {
     let setupScript = document.createElement('script');
     setupScript.type = 'text/javascript';
+    setupScript.id = 'wootric-settings';
     setupScript.async = true;
     setupScript.innerHTML = `
       wootric_no_surveyed_cookie = ${Defaults.WOOTRIC_NO_SURVEYED_COOKIE};
@@ -15,18 +16,23 @@ class Wootric extends React.Component {
         account_token: "${Defaults.WOOTRIC_ACCOUNT_TOKEN}"
       };
     `;
-    document.body.appendChild(setupScript);
+    if (document.getElementById('wootric-settings') == null) {
+      document.body.appendChild(setupScript);
+    }
 
     // Beacon
     let beacon = document.createElement('script');
     beacon.type = 'text/javascript';
+    beacon.id = 'wootric-beacon';
     beacon.async = true;
 
     beacon.src = 'https://cdn.wootric.com/wootric-sdk.js';
     beacon.onload = function() {
       window.wootric('run');
     };
-    document.body.appendChild(beacon);
+    if (document.getElementById('wootric-beacon') == null) {
+      document.body.appendChild(beacon);
+    }
   }
 
   render() {
